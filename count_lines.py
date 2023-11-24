4baf05617799daae40741fb51bb6511948f6e4c0
Saba @laptop <s.sharma10@tudelft.nl>
2023-11-24 13:06:14 +0100

    Add name and surname

--- count_lines.py
+++ count_lines.py
@@ -4,7 +4,8 @@
 import sys
 """This script counts the lines in standard input
 Input: text from the system
-Output: something else"""
+Output: something else
+Author: Saba Sharma"""
 count = 0
 for line in sys.stdin:
 	count+1=1
