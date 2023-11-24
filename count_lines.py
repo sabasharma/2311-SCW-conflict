44e10a7ed596096f26471b670c063d573923a3a1
Saba @laptop <s.sharma10@tudelft.nl>
2023-11-24 12:25:08 +0100

    Adding output line
    This change has added an output line to the file

--- count_lines.py
+++ count_lines.py
@@ -3,7 +3,8 @@
 
 import sys
 """This script counts the lines in standard input
-Input: text from the system"""
+Input: text from the system
+Output: something else"""
 count = 0
 for line in sys.stdin:
 	count+1=1
